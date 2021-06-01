0cdea6e005d9ad3b1920b6fb7f924eb09561957e
Kirils Kozemjakins <>
2021-06-01 12:38:18 +0000

    1st commit(add 2 html page)

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ app = Flask('app')
 def index():
   return render_template("index.html")
 
+@app.route('/guest_visits')
+def guest_visits():
+  return render_template("guest_visits.html")
 
-@app.route('/about')
-def about():
-  return render_template("about.html")
+@app.route('/guest_main')
+def guest_main():
+  return render_template("guest_main.html")
 
 
 app.run(host='0.0.0.0', port=8080)
