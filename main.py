08a53cb7d9d80634b99d09104fc70a4e6f246ed8
Kirils Kozemjakins <>
2021-06-01 13:41:12 +0000

    buttons

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask('app')
 
 @app.route('/')
 def index():
-  return render_template("index.html")
+  return render_template("guest_main.html")
 
 @app.route('/guest_visits')
 def guest_visits():
@@ -13,6 +13,7 @@ def guest_visits():
 @app.route('/guest_main')
 def guest_main():
   return render_template("guest_main.html")
+
 
 
 app.run(host='0.0.0.0', port=8080)
