b434859003a3456637f32f81af950a1ef1977532
Котович Костя <kostja.kotovich@gmail.com>
2022-02-15 09:54:30 +0200

    Pivienoju datus datu bāzē, pivienoju html lapas, sqllite-admin_hospital_page

--- app.py
+++ app.py
@@ -1,50 +1,31 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy import desc
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    title = db.Column(db.String(30), nullable=False)
+    content = db.Column(db.String(20), nullable=False)
+    title = db.Column(db.String(20), nullable=False)
+    article = db.Column(db.String(20), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return 'Task %r' % self.id
 
+class Slimnicas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nosaukums = db.Column(db.String(20), nullable=False)
+    adrese = db.Column(db.String(30), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('admin_doctor_page.html', tasks=tasks)
-
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
+    def __repr__(self):
+        return 'Slimnicas %r' % self.id
 
 @app.route('/')
 def sakum_login():
@@ -74,7 +55,104 @@ def admin_doctor_page():
 def admin_hospital_page():
   return render_template("admin_hospital_page.html")
 
+@app.route('/admin_statistika')
+def admin_statistika():
+  return render_template("admin_statistika.html")
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# Doctor page
+@app.route('/ok', methods=['POST', 'GET'])
+def doctors():
+    if request.method == 'POST':
+
+      new_task = Todo(content=request.form['content'], title=request.form['title'], article=request.form['article'])
+
+      try:
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect('/ok')
+      except:
+        return "che delat to"
+    else:
+      tasks = Todo.query.order_by(Todo.date_created).all()
+      return render_template("admin_doctor_page.html", tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/ok')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+        task.title = request.form['title']
+        task.article = request.form['article']
+
+        try:
+            db.session.commit()
+            return redirect('/ok')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update_doctor_page.html', task=task)
+
+# Slimnicas page
+@app.route('/ok2', methods=['POST', 'GET'])
+def slimnicas():
+    if request.method == 'POST':
+
+      new_slimnica = Slimnicas(nosaukums=request.form['nosaukums'], adrese=request.form['adrese'])
+
+      try:
+        db.session.add(new_slimnica)
+        db.session.commit()
+        return redirect('/ok2')
+      except:
+        return "Error"
+    else:
+      tasks2 = Slimnicas.query.order_by(Slimnicas.date_created).all()
+      return render_template("admin_hospital_page.html", tasks2=tasks2)
+
+
+@app.route('/delete2/<int:id>')
+def delete2(id):
+    slimnica_to_delete = Slimnicas.query.get_or_404(id)
+
+    try:
+        db.session.delete(slimnica_to_delete)
+        db.session.commit()
+        return redirect('/ok2')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update2/<int:id>', methods=['GET', 'POST'])
+def update2(id):
+    task = Slimnicas.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.nosaukums = request.form['nosaukums']
+        task.adrese = request.form['adrese']
+
+        try:
+            db.session.commit()
+            return redirect('/ok2')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update_hospital_page.html', task=task)
+
+
+if __name__ == "__main__": 
+  app.run(host='0.0.0.0', port=8080)
