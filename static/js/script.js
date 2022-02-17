776514068d29d3e15b6ed5a13e354ea245cb4b71
kkozemjakins <kkozemjakins@gmail.com>
2022-02-17 20:03:22 +0200

    add vizites

--- static/js/script.js
+++ static/js/script.js
@@ -6,53 +6,46 @@ window.addEventListener('load', () => {
     render();
 });
 
-document.getElementById("pievienotViziti").addEventListener('click', Poga)
-function Poga(){
-    if (datums.value === ""){
-        alert("Jūs neievadijāt vizītes datumu.")
-    };
-    if (laiks.value === ""){
-        alert("Jūs neievadijāt vizītes laiku.")
-    } else {
-        let vizite = {datums: datums.value, laiks: laiks.value};
-        datums.value = "";
-        laiks.value = "";
-    
-        vizites.push(vizite);
+document.getElementById('pievienotVizite').addEventListener('click', () => {
+    let vizite = {datums: datums.value, laiks: laiks.value};
 
-        render();
-    }
+    datums.value = "";
+    laiks.value = "";
 
-}
+    vizites.push(vizite);
+
+    render();
+})
 
 function render() {
     let saraksts = document.getElementById('saraksts');
     saraksts.innerHTML = "";
 
     for(let i = 0; i < vizites.length; i++) {
-
         let vizite = `
-    <li class="vizite">
-        <h3>Datums: ${vizites[i].datums}</h3>⠀⠀⠀<h4>Laiks: ${vizites[i].laiks}</h4>⠀⠀⠀⠀
-        <button class="del">Dzēst</button>
-    </li>`;
-    saraksts.innerHTML += vizite;
+        <div class="vizite">
+            <h3>Datums: ${vizites[i].datums}</h3>
+            <h3>Laiks: ${vizites[i].laiks}</h3>
+            <button class="del" onclick='removeViziti("${vizites[i].datums}")'>Dzēst</button>
+        </div>`;
+
+        saraksts.innerHTML += vizite;
     }
 
-    if(!localStorage.getItem('vizites')){
-      localStorage.setItem('vizites', JSON.stringify($scope.vizites));
-    } 
+    localStorage.setItem("vizites", JSON.stringify(vizites))
 }
 
-const list = document.querySelector('#saraksts')
 
-list.addEventListener('click', (e) => {
-    if(e.target.className == 'del'){
-      const li = e.target.parentElement;
-      li.parentNode.removeChild(li);
-      vizites.splice(li, 1);
-      if(!localStorage.getItem('vizites')){
-        localStorage.setItem('vizites', JSON.stringify($scope.vizites));
-      }
+function removeViziti(vizite){
+    for(let i = 0; i < vizites.length; i++) {
+        if(vizite === vizites[i].datums){
+            delete vizites[i];
+            break;
+        }
     }
-  });
+
+    vizites = vizites.filter(function (e) {return e != null;});
+
+    localStorage.setItem("vizites", JSON.stringify(vizites))
+    render();
+}
